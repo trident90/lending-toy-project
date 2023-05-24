4c69cd1d103622c8546382993cda9427fe11f9e9
Jongsik Jung <jongsik.jung@coinplug.com>
2023-05-24 15:39:20 +0900

    add test codes

--- test/payLoan.test.js
+++ test/payLoan.test.js
@@ -10,9 +10,9 @@ describe("Test LoanRequest", function () {
   let borrower;
   let lender;
 
-  const collateralAmount = "10"+"0".repeat(18);
-  const loanAmount = "1"+"0".repeat(18);
-  const payoffAmount = "2"+"0".repeat(18);
+  const collateralAmount = "200"+"0".repeat(18);
+  const loanAmount = "80"+"0".repeat(18);
+  const payoffAmount = "82"+"0".repeat(18);
   const loanDuration = 10000000;
 
   before(async () => {
@@ -40,14 +40,64 @@ describe("Test LoanRequest", function () {
   })
 
   it("borrower: approve token", async () => {
+    await collateralToken.connect(borrower).approve(
+      loanRequest.address,
+      collateralAmount,
+    );
   })
 
-  it("lender: deposit ether", async () => {
+  it("lender: lend ETH", async () => {
+    const prevTokenBalance1 = await collateralToken.balanceOf(borrower.address);
+    const prevTokenBalance2 = await collateralToken.balanceOf(lender.address);
+    const prevTokenBalance3 = await ethers.provider.getBalance(borrower.address);
+    const prevTokenBalance4 = await ethers.provider.getBalance(lender.address);
+    
+    await loanRequest.connect(lender).lendEther({value: loanAmount});
+    
+    const currTokenBalance1 = await collateralToken.balanceOf(borrower.address);
+    const currTokenBalance2 = await collateralToken.balanceOf(lender.address);
+    const currTokenBalance3 = await ethers.provider.getBalance(borrower.address);
+    const currTokenBalance4 = await ethers.provider.getBalance(lender.address);
+
+    console.log("previous borrower's token balance is", prevTokenBalance1/1e18);
+    console.log(" current borrower's token balance is", currTokenBalance1/1e18);
+    console.log("previous lender's token balance is", prevTokenBalance2/1e18);
+    console.log(" current lender's token balance is", currTokenBalance2/1e18);
+    console.log("previous borrower's ETH balance is", prevTokenBalance3/1e18);
+    console.log(" current borrower's ETH balance is", currTokenBalance3/1e18);
+    console.log("previous lender's ETH balance is", prevTokenBalance4/1e18);
+    console.log(" current lender's ETH balance is", currTokenBalance4/1e18);
+
+    expect(prevTokenBalance1.sub(currTokenBalance1)).to.be.equal(collateralAmount);
   })
 
   it("get loan contract", async () => {
+    const loanAddress = await loanRequest.loan();
+    loan = await ethers.getContractAt("Loan", loanAddress);
+
+    console.log("loan: ", loan.address);
   })
 
-  it("borrower: repay ether", async () => {
+  it("borrower: repay ETH", async () => {
+    const prevTokenBalance1 = await collateralToken.balanceOf(borrower.address);
+    const prevTokenBalance2 = await collateralToken.balanceOf(lender.address);
+    const prevTokenBalance3 = await ethers.provider.getBalance(borrower.address);
+    const prevTokenBalance4 = await ethers.provider.getBalance(lender.address);
+    
+    await loan.connect(borrower).payLoan({value: payoffAmount});
+    
+    const currTokenBalance1 = await collateralToken.balanceOf(borrower.address);
+    const currTokenBalance2 = await collateralToken.balanceOf(lender.address);
+    const currTokenBalance3 = await ethers.provider.getBalance(borrower.address);
+    const currTokenBalance4 = await ethers.provider.getBalance(lender.address);
+
+    console.log("previous borrower's token balance is", prevTokenBalance1/1e18);
+    console.log(" current borrower's token balance is", currTokenBalance1/1e18);
+    console.log("previous lender's token balance is", prevTokenBalance2/1e18);
+    console.log(" current lender's token balance is", currTokenBalance2/1e18);
+    console.log("previous borrower's ETH balance is", prevTokenBalance3/1e18);
+    console.log(" current borrower's ETH balance is", currTokenBalance3/1e18);
+    console.log("previous lender's ETH balance is", prevTokenBalance4/1e18);
+    console.log(" current lender's ETH balance is", currTokenBalance4/1e18);
   })
 });
